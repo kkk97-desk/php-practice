42dd991b706e4adcab0c7316970bd2883eedcdd1
kentodaikoku <k-daikoku@athuman.com>
2022-07-14 14:40:11 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>XAMPP</h1>
+    
+    <!-- phpの情報を開示 -->
+    <!-- <?php phpinfo(); ?> -->
+
+    <!-- マジック定数 -->
+    <div>
+        <ul>
+            <li class="a">この桁<?= __LINE__ ?></li>
+            <li class="a">ファイルパス<?= __FILE__ ?></li>
+            <li class="a">ディレクトリパス<?= __DIR__ ?></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,31 @@
+<?php
+
+// echo '<script>alert("ようこそWEBの世界へ")</script>';
+
+function alert() {
+    echo '<script>alert("ようこそWEBの世界へ")</script>';
+}
+
+alert();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </div>
+</body>
+</html>
